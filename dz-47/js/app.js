e17ac702a4636d93b2a553f217e223deb85eefbe
Коля Кролевецкий <papa.zajchishki@gmail.com>
2024-01-04 23:45:45 +0200

    dz47

--- dz-47/js/app.js
+++ dz-47/js/app.js
@@ -1,40 +1,14 @@
-//Обработка ошибок
-/*
-const fnWithError = () => {
-    throw new Error('Some error 404 uk')
-}
-try{
-fnWithError()
-} catch (error){
-    console.error(error)
-    console.log(error.message)
-}
-console.log('continue')
+var codes = {};
 
-*/
+var cod1 = " 6";
+codes[cod1] = "Польща";
+ 
+var cod2 = " 38";
+codes[cod2] = "Україна"; 
 
-let jsonString1 = '{"name": "John", "age": 30, "city": "New York"}';
-
-let user1 = JSON.parse(jsonString1);
-
-console.log( user1 );
-
-
-
-let jsonString2 =  '{"name": "Alice", "age": "twenty-five", "city": "London"}';
-
-try {
-
-  let user2 = JSON.parse(jsonString2);
-  if (age !== 'string') {
-    console.log(user2)
-  }
-
-} catch (err) {
-  console.log( "Invalid JSON: Unexpected string in JSON at position 27" );
-}
-
-
+var cod3 =  "_1";
+codes[cod3] = "США"; 
+console.log(codes);
 
 
 
